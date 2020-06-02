c316e509d14655989d83c84b1831a504163f8ae9
frantisek.hallo <fantomx1@gmail.com>
2020-06-02 20:56:48 +0200

    dashboard of the user

---
+++ dashboard.php
@@ -0,0 +1,28 @@
+<?php
+// dashboard.php <user-id>
+
+require_once "bootstrap.php";
+
+$theUserId = $argv[1];
+
+
+// b shortcut is what joins to what
+$dql = "SELECT b, e, r FROM Bug b JOIN b.engineer e JOIN b.reporter r ".
+        "WHERE b.status ='OPEN' AND (e.id = ?1 OR r.id = ?1 ) ORDER BY b.created DESC";
+
+
+
+$myBugs = $entityManager = $entityManager->createQuery($dql)
+                                         ->setParameter(1, $theUserId)
+                                         ->setMaxResults(15)
+                                         ->getResult();
+
+echo "You have created or assigned to ". count($myBugs) . " open bug: \n\n";
+
+
+foreach ($myBugs as $bug) {
+
+    echo $bug->getId() . "-". $bug->getDescription()."\n";
+
+
+}
